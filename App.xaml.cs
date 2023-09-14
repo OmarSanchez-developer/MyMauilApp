7376f008d63f998e816ed1cb8b5d14c42f0c9c9d
Ing. Omar Sanchez <ing_omarglez@hotmail.com>
2023-09-14 08:49:51 -0600

    Creando mi primer aplicacion con .NET. MVC MVVM
    Blazer , comprendiendo la lógica de la aplicación

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MyMauilApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
+
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MyMauilApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
